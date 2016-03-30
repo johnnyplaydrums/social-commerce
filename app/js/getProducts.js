125040ed5968e86428d6ebef21b829e0b413aa6a
John Spencer <johnspencer15@gmail.com>
2016-03-30 12:30:01 -0400

    add project skeleton, npm modules and bower dependencies, implemented: product listing, add products, edit products

---
+++ app/js/getProducts.js
@@ -0,0 +1,79 @@
+$(function() {
+  
+  //ajax call to load products
+  // GET /getProducts
+  $.ajax({
+    type : 'GET',
+    url : '/getProducts'
+  }).success(function(response) {
+    displayProducts(response);
+  }).fail(function(xhr, status, message) { 
+    toastr.error('Something went wrong!');
+  });
+  
+  function displayProducts(response) {
+    var container = $('.products-field .container'),
+        col,
+        panel,
+        heading,
+        body,
+        img,
+        rating,
+        help,
+        detailsButton,
+        editButton,
+        i,
+        j;
+    
+    for (i = 0; i < response.length; i++) {
+      col = $('<div>')
+        .addClass('col-sm-4 col-lg-3');
+      panel = $('<div>')
+        .addClass('panel panel-default');
+      heading = $('<div>')
+        .addClass('panel-heading')
+        .text(response[i].name);
+      body = $('<div>')
+        .addClass('panel-body');
+      img = $('<img>')
+        .addClass('product-image')
+        .attr('src', response.img ? response.img : '/img/placeholder.jpg');
+      rating = $('<p>')
+        .addClass('starRating display')
+        .append($('<span class="star"></span>'),$('<span class="star"></span>'),
+                $('<span class="star"></span>'),$('<span class="star"></span>'),
+                $('<span class="star"></span>'));
+      help = $('<span>')
+        .addClass('help-block');
+      detailsButton = $('<a>')
+        .addClass('btn btn-primary')
+        .text('Details')
+        .attr('href', '/product?' + response[i]._id);
+      editButton = $('<a>')
+        .addClass('btn btn-primary')
+        .css('float', 'right')
+        .text('Edit')
+        .attr('href', '/editProduct?id=' + response[i]._id);
+      
+      for (j = 0; j < 5; j++) {
+        if (j >= response[i].averageRating) {
+          $(rating.children()[j])
+            .append(
+              $('<img>').attr('src', '/fonts/star-off.svg')
+            );
+        } else {
+          $(rating.children()[j])
+            .append(
+              $('<img>').attr('src', '/fonts/star-on.svg')
+            );
+        }
+      }
+      
+      body.append(img, rating, help, detailsButton, editButton);
+      panel.append(heading, body);
+      col.append(panel);
+      container.append(col);
+    }
+  }
+  
+});
